f9522c6943274ef0e3feb93be02fb83604a82acf
Ezequiel Rubio <erubio07@gmail.com>
2023-07-01 11:57:52 -0300

    link globe in navbar

--- client/countries/src/Components/Navbar/Navbar.jsx
+++ client/countries/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import globe from "./globe.png";
 
 function Navbar() {
   return (
     <div className={styles.container}>
+      <NavLink className={styles.globeContainer} to="/">
+        <img className={styles.globe} src={globe} alt="globe" />
+      </NavLink>
       <NavLink className={styles.link} to="/home">
         Home
       </NavLink>
