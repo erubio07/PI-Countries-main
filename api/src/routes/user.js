15f98d427e8e864bf57dc49719756046534ed5bc
Ezequiel Rubio <erubio07@gmail.com>
2023-08-14 13:17:06 -0300

    sigo con signup

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -3,4 +3,12 @@ const { singUp } = require("../Controllers/singup");
 
 const router = Router();
 
-router.post("/", singUp);
+router.post("/", async (req, res) => {
+  const { name, username, password } = req.body;
+  try {
+    const newUser = await singUp(name, username, password);
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
